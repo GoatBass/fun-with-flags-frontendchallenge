b97bc36b91ca04b1c5cdf4919602489a70038634
Edu <edugacv@gmail.com>
2022-09-12 12:38:59 +0200

    modified fecth url value for the new ones so the app works again

--- js/request.js
+++ js/request.js
@@ -1,14 +1,14 @@
 
 const Requests = {
     getCountriesFromJSON(cb){
-        const url = 'https://restcountries.eu/rest/v2/all'
+        const url = 'https://restcountries.com/v2/all'
         fetch(url).then(response => response.json()).
         then(result => cb(result)).
         catch(error => console.log(error))
     },
 
     getCountriesByAlphaCode(alphaCode, cb){
-        const url = 'https://restcountries.eu/rest/v2/alpha/' + alphaCode
+        const url = 'https://restcountries.com/v2//alpha/' + alphaCode
         fetch(url).then(response => response.json()).
         then(result => cb(result)).
         catch(error => console.log(error))
